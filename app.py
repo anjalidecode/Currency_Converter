f0a38db02440bb2461ea407613c37215fde0168c
Anjali Sharma <anjalidecodes@gmail.com>
2026-05-09 20:40:20 +0530

    Integrated ExchangeRate API with Flask app

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from flask import Flask
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("API_KEY")
+
+BASE_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/"
 
 
 @app.route("/")
 def home():
 
-    return "Flask App is Running!"
+    from_currency = "USD"
+
+    to_currency = "INR"
+
+    amount = 1
+
+    url = BASE_URL + from_currency
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    rate = data["conversion_rates"][to_currency]
+
+    converted_amount = round(amount * rate, 2)
+
+    return f"{amount} {from_currency} = {converted_amount} {to_currency}"
 
 
 if __name__ == "__main__":
